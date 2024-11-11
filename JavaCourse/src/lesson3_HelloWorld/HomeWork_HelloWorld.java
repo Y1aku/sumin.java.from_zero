084baebbb2acb8ab02d9c52f483a89ce5d045795
Kirill <stoporenko.k@gmail.com>
2024-11-12 01:53:13 +0700

    solved homework for HelloWorld lesson

---
+++ JavaCourse/src/lesson3_HelloWorld/HomeWork_HelloWorld.java
@@ -0,0 +1,9 @@
+package lesson3_HelloWorld;
+
+public class HomeWork_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Dad: Mike");
+        System.out.println("Mom: Helen");
+        System.out.println("Son: Jeff");
+    }
+}
